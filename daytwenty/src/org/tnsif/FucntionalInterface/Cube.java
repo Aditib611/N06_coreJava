5805de945f42879e1338ee85fb9738639015474b
Aditi Bhavsar <97657912+Aditib611@users.noreply.github.com>
2024-01-08 20:56:09 +0530

    first commit

---
+++ daytwenty/src/org/tnsif/FucntionalInterface/Cube.java
@@ -0,0 +1,9 @@
+package org.tnsif.FucntionalInterface;
+
+@FunctionalInterface
+public interface Cube {
+	int getCube(int no);
+	//int getSquare(int no); //CTE 
+	
+
+}
---
+++ daytwenty/src/org/tnsif/FucntionalInterface/Operations.java
@@ -0,0 +1,6 @@
+package org.tnsif.FucntionalInterface;
+
+@FunctionalInterface
+public interface Operations {
+	float performArithmetic(int a, int b);
+}
